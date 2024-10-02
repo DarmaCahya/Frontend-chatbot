e3291b6a8f50a91f929182621255591268e12ebb
Darma Cahya <darmady6@gmail.com>
2024-10-03 04:07:16 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class", 
+  content: ['./*/*.html', ],
+  purge: [
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
