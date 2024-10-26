38611a3d79cbe9bfaa6ce1b1880e8d71681d3809
Darma Cahya <darmady6@gmail.com>
2024-10-26 19:23:58 +0700

    add api register

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const path = require('path');
 const axios = require('axios');
+require('dotenv').config();
 
 const app = express();
+app.use(express.json());
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -16,10 +18,6 @@ app.get('/opening', (req, res) => {
     res.render('opening');
 });
 
-app.get('/login', (req, res) => {
-    res.render('login');
-});
-
 app.get('/chat', (req, res) => {
     res.render('chatAI');
 });
@@ -28,8 +26,56 @@ app.get('/', (req, res) => {
     res.render('test');
 });
 
+// authentikasi API
 app.get('/register', (req, res) => {
     res.render('register')
+})
+app.post('/api/register', async(req,res) => {
+    console.log(req.username);
+    try {
+        const response = await fetch('http://ec2-35-85-179-20.us-west-2.compute.amazonaws.com/user/api/auth/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(req.body),
+        });
+    
+        // Log status dan body respons
+        console.log('Status Code:', response.status);
+        console.log('Response Body:', await response.text());
+    
+        const data = await response.json();
+        res.status(response.status).json(data);
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ message: "Terjadi kesalahan saat melakukan registrasi." });
+    }
+    
+})
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+app.post ('/api/login', async(req,res) => {
+    try {
+        const response = await fetch ('http://ec2-35-85-179-20.us-west-2.compute.amazonaws.com/api/auth/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(req.body),
+        });
+        const data = await response.json();
+        res.status(response.status).json(data);
+    }catch (error) {
+        res.status(response.status).json(data);
+        res.status(500).json({ message: "Terjadi kesalahan saat melakukan registrasi." });
+    }
+})
+
+app.get('/response', (req, res) => {
+    res.render('response')
 })
 
 app.listen(PORT, () => {
