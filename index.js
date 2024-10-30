0fdcae8fcb400823ce89f3f397a097888c998353
Darma Cahya <darmady6@gmail.com>
2024-10-30 23:39:43 +0700

    add decode token

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const axios = require('axios');
 require('dotenv').config();
+const jwt_decode = require('jwt-decode');
 
 const app = express();
 app.use(express.json());
@@ -18,12 +19,39 @@ app.get('/opening', (req, res) => {
     res.render('opening');
 });
 
-app.get('/chat', (req, res) => {
+app.get('/chats', (req, res) => {
     res.render('chatAI');
 });
 
 app.get('/', (req, res) => {
     res.render('test');
+});
+
+// API Chat
+app.post('/history', async (req, res) => {
+    try{
+        const {historyName, status, userId} = req.body;
+
+        const response = await fetch(process.env.HISTORY_API_URL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ historyName,status, userId })
+        })
+        const contentType = response.headers.get("content-type");
+        if (contentType && contentType.includes("application/json")) {
+            const data = await response.json();
+            console.log(data);
+            res.status(response.status).json(data);
+        } else {
+            const text = await response.text();
+            res.status(response.status).json({ message: text });
+        }
+    }catch (error) {
+        console.error("Error:", error);
+        res.status(500).json({ message: "Terjadi kesalahan saat membuat history." });
+    }
 });
 
 app.post('/bot', async (req,res ) => {
@@ -109,7 +137,6 @@ app.post('/api/login', async (req, res) => {
         res.status(500).json({ message: "Terjadi kesalahan saat melakukan masuk." });
     }
 });
-
 app.get('/verify', (req, res) => {
     res.render('verify')
 })
