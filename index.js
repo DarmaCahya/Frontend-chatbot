2d40fe39d970c563bcc09bcad32ba90322d0849d
Darma Cahya <darmady6@gmail.com>
2024-11-02 16:04:52 +0700

    add history API

--- index.js
+++ index.js
@@ -31,7 +31,6 @@ app.get('/', (req, res) => {
 app.post('/histories', async (req, res) => {
     try{
         const {historyName, status, userId, token} = req.body;
-
         const response = await fetch(process.env.HISTORY_API_URL, {
             method: 'POST',
             headers: {
@@ -43,7 +42,6 @@ app.post('/histories', async (req, res) => {
         const contentType = response.headers.get("content-type");
         if (contentType && contentType.includes("application/json")) {
             const data = await response.json();
-            console.log(data);
             res.status(response.status).json(data);
         } else {
             const text = await response.text();
@@ -54,11 +52,86 @@ app.post('/histories', async (req, res) => {
         res.status(500).json({ message: "Terjadi kesalahan saat membuat history." });
     }
 });
+app.delete('/histories/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const token = req.headers.authorization;
+
+        const response = await fetch(`${process.env.HISTORY_API_URL}/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            }
+        });
+
+        const contentType = response.headers.get("content-type");
+        if (contentType && contentType.includes("application/json")) {
+            const data = await response.json();
+            console.log("Deleted data:", data);
+            res.status(response.status).json(data);
+        } else {
+            const text = await response.text();
+            res.status(response.status).json({ message: text });
+        }
+    } catch (error) {
+        console.error("Error:", error);
+        res.status(500).json({ message: "Terjadi kesalahan saat menghapus history." });
+    }
+});
+app.get('/history-lists/:userId', async (req, res) =>{
+    try{
+        const historyList = process.env.HISTORYLIST_API_URL;
+        const token = req.headers.authorization;
+        const userId = req.params.userId;
+        const response = await fetch(`${historyList}${userId}`,{
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            }
+        })
+        if (!response.ok) {
+            throw new Error('Gagal mengambil daftar chat');
+        }
+        const data = await response.json(); 
+        res.status(200).json(data);
+    }catch (error) {
+        console.error(error);
+    }
+})
+app.put('/histories/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const token = req.headers.authorization;
+        const {historyName, status, userId} = req.body;
+
+        const response = await fetch(`${process.env.HISTORY_API_URL}/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            },
+            body: JSON.stringify({ historyName, status, userId})
+        });
+
+        const contentType = response.headers.get("content-type");
+        if (contentType && contentType.includes("application/json")) {
+            const data = await response.json();
+            res.status(response.status).json(data);
+        } else {
+            const text = await response.text();
+            res.status(response.status).json({ message: text });
+        }
+    } catch (error) {
+        console.error("Error:", error);
+        res.status(500).json({ message: "Terjadi kesalahan saat memperbarui history." });
+    }
+});
 
 app.post('/bot', async (req,res ) => {
     try {
         const { message, token_jwt, historyId } = req.body; 
-        console.log('history', historyId);
         const response = await fetch(process.env.BOT_API_URL, {
             method: 'POST',
             headers: {
