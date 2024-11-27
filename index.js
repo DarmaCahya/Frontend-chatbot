74d33af62f229cde48aae9abb1dfe0d61cc0a1c0
Darma Cahya <darmady6@gmail.com>
2024-11-27 18:20:43 +0700

    add verify email for register

--- index.js
+++ index.js
@@ -466,6 +466,32 @@ app.get('/user-active', async (req, res) =>{
     }
 })
 
+app.get('/verify-email', async (req, res) =>{
+    try{
+        const urlEmailVerify = process.env.URL_VERIFICATION_EMAIL;
+        const verifyEmailToken = process.env.KEY_HUNTER;
+        const email = req.query.email;
+        if (!email) {
+            return res.status(400).json({ error: 'Email query parameter is required' });
+        }
+        const verifyEmail = `${urlEmailVerify}?email=${email}&api_key=${verifyEmailToken}`
+        const response = await fetch( verifyEmail,{
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        });
+        if (!response.ok) {
+            const errorData = await response.json();
+            return res.status(response.status).json({ error: errorData });
+        }
+        const data = await response.json();
+        res.status(200).json(data);
+
+    }catch (error) {
+        console.error(error);
+    }
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
